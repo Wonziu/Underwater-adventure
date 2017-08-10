1bfacac75649d456d7d3d682622186e5c4c9de74
Wonziu <leonwonziu@gmail.com>
2017-08-11 00:06:21 +0200

    Added Sprites and paralax scrolling

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(moveHorizontal, moveVertical) / 20);
+
+	    Camera.main.transform.position = new Vector3(transform.position.x, 0, -5);
+	}
+}
